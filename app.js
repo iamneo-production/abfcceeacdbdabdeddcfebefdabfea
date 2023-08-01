281fd846945adc9ced868bf9e7f22fc18494bf85
Iamneo <techadmin@iamneo.ai>
2023-08-01 13:54:29 +0000

    da

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("fdsafd")
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("hiii")
+}
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("hiii")
+}
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("hiii")
+}
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("hiii")
+}
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("hiii")
+}
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("hiii")
+}
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("hiii")
+}
